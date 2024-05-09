a72672ce455cc5b012cb3dbb4cb3d1a851200c49
CuchoLean <leandrocucho16@gmail.com>
2024-05-09 12:30:50 +0200

    Con readers and main.

--- src/test/java/org/ies/library/models/LibraryTest.java
+++ src/test/java/org/ies/library/models/LibraryTest.java
@@ -13,8 +13,8 @@ import java.util.List;
 public class LibraryTest {
     private Book book = new Book("1", "Libro1", "Autor1", List.of("Fantasía", "Horror"));
     private Book book2 = new Book("2", "Libro2", "Autor2", List.of("Drama", "Mal"));
-    private Lend lend = new Lend("1", new Date(), "12");
-    private Lend lend2 = new Lend("2", new Date(), "13");
+    private Lend lend = new Lend("1", new Date(), "12",new Date());
+    private Lend lend2 = new Lend("2", new Date(), "13",new Date());
     private Member member1 = new Member("12", "Antonio", "Q", 123, 12345);
     private Member member2 = new Member("13", "Iker", "G", 456, 12345);
     private Library library = new Library("Libreria", List.of(book, book2), List.of(member1, member2), List.of(lend, lend2));
